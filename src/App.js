50b7073a2a3e204e2002f20a35e4d5a1d6f2ff38
João Pedro Resende <jpresende502@gmail.com>
2022-04-07 15:41:06 -0300

    adding project code and him assets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
+import moment from "moment";
+import styles from "./styles.module.css"
+import CesinhaImg from "./assets/cesinha.jpg"
+import CesinhaAudio from "./assets/cesinhaaudio.mp3"
 
 function App() {
+  const { width, height } = useWindowSize()
+
+  const date = {
+    todayDate: moment(moment().format("YYYY-MM-DD h:d")),
+    nextFriday: moment(moment().day("Friday").format("YYYY-MM-DD 18:00"))
+  }
+
+  const time = date.nextFriday - date.todayDate
+  const [remainingDays, setRemainingDays] = useState(moment.duration(time).days())
+  const [remainingHours, setRemainingHours] = useState(moment.duration(time).hours())
+  const [remainingMinutes, setRemainingMinutes] = useState(moment.duration(time).minutes())
+
+  const song = new Audio(CesinhaAudio);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.middleContainer}>
+      <nav>
+        {
+          remainingDays != 0 ?
+          <h1 style={{color: "mediumblue", textDecoration: "underline"}} onClick={() => song.play()}>Calma cesinha!</h1>
+          :
+          <h1>SEXTOU CESINHA!</h1>
+
+        }
+        <img src={CesinhaImg} />
+      </nav>
+      <section className={styles.flex}>
+        <audio autoPlay={true}>
+          <source src={CesinhaAudio} type="audio/mp3" />
+          Your browser does not support the audio element.
+        </audio> 
+        {
+          remainingDays == 0 ?
+          ""
+          :
+          <h1>SEXTOU CESINHA!</h1>
+        }
+        <section className={styles.countdownContainer}>
+          <div>
+            <div className={styles.counter}>
+              {remainingDays}
+            </div>
+            <p>{remainingDays == 1 ? "Dia" : "Dias"}</p>
+          </div>
+          <div>
+            <div className={styles.counter}>
+              {remainingHours}
+            </div>
+            <p>{remainingHours == 1 ? "Hora" : "Horas"}</p>
+          </div>
+          <div>
+            <div className={styles.counter}>
+              {remainingMinutes}
+            </div>
+            <p>{remainingMinutes == 1 ? "Minuto" : "Minutos"}</p>
+          </div>
+        </section>
+        {
+          remainingDays != 0 &&
+          <h1>Para sexta feira 18:00!</h1>
+        }
+      </section>
+      {
+        remainingDays == 0 &&  
+        <Confetti
+          width={width}
+          height={height}
+        />
+      }
     </div>
   );
 }
