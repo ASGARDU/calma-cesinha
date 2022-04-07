c9b9b43cdd0716d25242a610c2a50f3d9c1725aa
João Pedro Resende <jpresende502@gmail.com>
2022-04-07 18:00:26 -0300

    bug fixes and adding new features

--- src/App.js
+++ src/App.js
@@ -6,39 +6,48 @@ import styles from "./styles.module.css"
 import CesinhaImg from "./assets/cesinha.jpg"
 import CesinhaBraboImg from "./assets/cesinhabrabo.jpg"
 import CesinhaAudio from "./assets/cesinhaaudio.mp3"
+import SextouAudio from "./assets/sextouaudio.mp3"
 
 function App() {
   const { width, height } = useWindowSize()
 
   const date = {
-    todayDate: moment(moment().format("YYYY-MM-DD h:m")),
-    nextFriday: moment(moment().day("Friday").format("YYYY-MM-DD 18:00"))
+    todayDate: moment(moment().format("YYYY-MM-DD h:m")).locale("pt-br"),
+    nextFriday: moment(moment().day("Friday").format("YYYY-MM-DD 18:00")).locale("pt-br")
   }
 
+  const [angryCesinhaAudio, setCesinhaAudio] = useState(new Audio(CesinhaAudio))
+  const [sextouAudio, setSextouAudio] = useState(new Audio(SextouAudio))
+
   const time = date.nextFriday - date.todayDate
+
   const [remainingDays, setRemainingDays] = useState(moment.duration(time).days())
   const [remainingHours, setRemainingHours] = useState(moment.duration(time).hours())
   const [remainingMinutes, setRemainingMinutes] = useState(moment.duration(time).minutes())
 
-  const song = new Audio(CesinhaAudio);
-
+  
   useEffect(() => {
-    song.play()
+    if(remainingDays >  0 && remainingHours > 0 && remainingMinutes > 0) {
+      angryCesinhaAudio.play()
+    }
+    if(remainingDays <= 0 && remainingHours <= 0 && remainingMinutes <= 0) {
+      sextouAudio.play()
+    }
   }, [])
 
   return (
     <div className={styles.middleContainer}>
       <nav>
         {
-          (remainingDays && remainingHours && remainingHours) != 0 ?
-          <h1 style={{color: "mediumblue", textDecoration: "underline"}} onClick={() => song.play()}>Calma cesinha!</h1>
-          :
+          (remainingDays <=  0 && remainingHours <= 0 && remainingMinutes <= 0) ?
           <h1>SEXTOU CESINHA!</h1>
+          :
+          <h1 style={{color: "mediumblue", textDecoration: "underline"}} onClick={() => angryCesinhaAudio.play()}>Calma cesinha!</h1>
 
         }
         {
-          (remainingDays && remainingHours && remainingHours) != 0 ?
-          <img src={CesinhaImg} />
+          (remainingDays >  0 && remainingHours > 0 && remainingMinutes > 0) ?
+          <img src={CesinhaImg}/>
           :
           <img src={CesinhaBraboImg} />
         }
@@ -49,42 +58,46 @@ function App() {
           Your browser does not support the audio element.
         </audio> 
         {
-          (remainingDays && remainingHours && remainingHours) != 0 ?
+          (remainingDays >  0 && remainingHours > 0 && remainingMinutes > 0) ?
           <h1>Ainda falta</h1>
           :
-          <h1>SEXTOU CESINHA!</h1>
+          <h1>Aproveita!</h1>
         }
         <section className={styles.countdownContainer}>
           <div>
             <div className={styles.counter}>
-              {remainingDays}
+              {remainingDays < 0 ? 0 : remainingDays}
             </div>
             <p>{remainingDays == 1 ? "Dia" : "Dias"}</p>
           </div>
           <div>
             <div className={styles.counter}>
-              {remainingHours}
+              {remainingHours < 0 ? 0 : remainingHours}
             </div>
             <p>{remainingHours == 1 ? "Hora" : "Horas"}</p>
           </div>
           <div>
             <div className={styles.counter}>
-              {remainingMinutes}
+              {remainingMinutes < 0 ? 0 : remainingMinutes}
             </div>
             <p>{remainingMinutes == 1 ? "Minuto" : "Minutos"}</p>
           </div>
         </section>
         {
-          remainingDays != 0 &&
+          (remainingDays > 0 && remainingHours > 0 && remainingMinutes > 0) ?
           <h1>Para sexta feira 18:00!</h1>
+          :
+          ""
         }
       </section>
       {
-        (remainingDays && remainingHours && remainingHours) == 0 &&  
+        (remainingDays <= 0 && remainingHours <= 0 && remainingMinutes <= 0) ?  
         <Confetti
           width={width}
           height={height}
         />
+        :
+        ""
       }
     </div>
   );
